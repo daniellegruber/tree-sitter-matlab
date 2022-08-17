bd77de112823ef1b0dc56fb9aca80ecdb707476b
Danielle Lauren Gruber <danielle.l.gruber@gmail.com>
2022-08-17 12:40:49 -0400

    Updated files

--- example.m
+++ example.m
@@ -1,7 +1,40 @@
-[x, j] = func_call();
-[x, j] = func_call(as, as, sa, s);
-x = func_call(saa, sa);
-x = 3
+A = [1, 2;
+    3, 4];
+B = [A;A];
+C = B*A;
 
-function ads()
+a = 2.5;
+c = a < b;
+
+D = {'hello', 'world'};
+E = D';
+
+[F,G] = myfun1(1,2);
+
+myvar = A(1,1);
+
+% This is a comment
+
+if a
+  b
+elseif c
+  d
+else
+  f
+  
+for i = 1:5
+    a;
+end
+
+for i = [2, 7, 9]
+    a;
+end
+
+function [F, G] = myfun1(f,g)
+    F = f + g;
+    G = f - g;
+end
+
+function outstr = myfun2()
+    outstr = "hello world";
 end
